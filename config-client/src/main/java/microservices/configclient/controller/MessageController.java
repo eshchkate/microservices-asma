97ca9569de1fbf5c291a39a0b2f88b627b5fa21f
Ekaterina <trfnthbyf997@gmail.com>
2019-06-06 01:01:38 +0600

    ms vs spring

--- config-client/src/main/java/microservices/configclient/controller/MessageController.java
+++ config-client/src/main/java/microservices/configclient/controller/MessageController.java
@@ -1,4 +1,4 @@
-package microservices.configclient;
+package microservices.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
 @RestController
-public class MessageResource {
+public class MessageController {
 
     @Value("${app.id}")
-//    private String message;
-            String instance;
+    String instance;
 
     @GetMapping("/")
     public String message() {
--- ribbon-client/src/main/java/microservices/ribbonclient/controller/RibbonController.java
+++ ribbon-client/src/main/java/microservices/ribbonclient/controller/RibbonController.java
@@ -1,5 +1,8 @@
 package microservices.ribbonclient.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +14,20 @@ public class RibbonController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "getFallbackConfigClient",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+            })
     @RequestMapping("/")
     public String callService() {
+        if (RandomUtils.nextBoolean())
+            throw new RuntimeException("Failed!");
         return restTemplate.getForEntity("http://config-client", String.class).getBody();
+    }
 
+    public String getFallbackConfigClient() {
+        return "Service is unavailable";
     }
 
 }
