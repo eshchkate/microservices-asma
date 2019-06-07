e386d4c6ffb7efc98c1a8c07327329ae72700e57
Ekaterina <trfnthbyf997@gmail.com>
2019-06-08 03:16:52 +0600

    ms vs spring

---
+++ oauth2-client/src/test/java/microservices/oauth2client/Oauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.oauth2client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2ClientApplicationTests {
+
+
+}
